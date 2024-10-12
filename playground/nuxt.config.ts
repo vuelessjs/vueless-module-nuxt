e07c29916768cbadd6cb6ee010b8d40074ec2db3
Johnny Grid <ilevel.ws@gmail.com>
2024-10-12 13:22:17 +0800

    Fixed module registration.

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-09-13',
-  modules: ['@vueless/module-nuxt'],
+  modules: ['../src/module.ts'],
   devtools: { enabled: true },
   tailwindcss: {
     config: {
