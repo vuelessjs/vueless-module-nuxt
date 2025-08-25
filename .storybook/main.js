de3886e2daa8c2a13957a149e32c0fde7cf20e1e
Johnny Grid <ilevel.ws@gmail.com>
2025-08-25 16:11:36 +0800

    Refactored Storybook configuration: moved `basePath` from `main.js` to `vite.config.js` for consistency.

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,6 @@
 import { defineConfigWithVueless } from "vueless/storybook";
 
 export default defineConfigWithVueless({
-  basePath: "playground",
   stories: [
     /* Define a path to your own component stories. */
     // "../src/**/stories.{js,jsx,ts,tsx}",
